80de1746f50e02c19a92dea86541b86a255d4756
Wodanson <1782819gillissen@zuyd.nl>
2022-12-02 10:43:15 +0100

    made connection to rasa server

---
+++ Assets/Augmented Reality Assistant/_Scripts/NetworkManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+// A struct to help in creating the Json object to be sent to the rasa server
+public class PostMessageJson {
+    public string message;
+    public string sender;
+}
+
+public class NetworkManager : MonoBehaviour {
+
+    private const string rasa_url = "http://localhost:5005/webhooks/rest/webhook";
+
+    public void SendMessageToRasa () {
+        // Create a json object from user message
+        PostMessageJson postMessage = new PostMessageJson {
+            sender = "user",
+            message = "Hi"
+        };
+
+        string jsonBody = JsonUtility.ToJson(postMessage);
+        print("User json : " + jsonBody);
+
+        // Create a post request with the data to send to Rasa server
+        StartCoroutine(PostRequest(rasa_url, jsonBody));
+    }
+
+    private IEnumerator PostRequest (string url, string jsonBody) {
+        UnityWebRequest request = new UnityWebRequest(url, "POST");
+        byte[] rawBody = new System.Text.UTF8Encoding().GetBytes(jsonBody);
+        request.uploadHandler = (UploadHandler)new UploadHandlerRaw(rawBody);
+        request.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+        request.SetRequestHeader("Content-Type", "application/json");
+
+        yield return request.SendWebRequest();
+
+        Debug.Log("Response: " + request.downloadHandler.text);
+    }
+}
