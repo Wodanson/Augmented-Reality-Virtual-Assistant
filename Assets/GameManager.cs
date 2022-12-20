2102b9d79ab279fd0f327f445394930f8352b41c
Wodanson <60174582+Wodanson@users.noreply.github.com>
2022-12-20 18:43:10 +0100

    test

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -3,16 +3,15 @@ using System.Collections.Generic;
 using Unity.VisualScripting.InputSystem;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public int maxMessages = 25;
-
+    //private const string rasa_url = "http://localhost:5005/webhooks/rest/webhook";
     public GameObject chatPanel, textObject;
     public InputField chatBox;
-    public TouchScreenKeyboard Keyboard;
-    public string text = "";
 
     [SerializeField]
     List<Message> MessageList = new List<Message>();
@@ -25,24 +24,28 @@ public class GameManager : MonoBehaviour
     }
     void Update()
     {
-        if ( Keyboard.status == TouchScreenKeyboard.Status.Done)
+        if (chatBox.text != "")
         {
-            SendMessageToChat("test");
-            if (chatBox.text != "")
+            if(Input.GetKeyDown(KeyCode.Return))
             {
                 SendMessageToChat(chatBox.text);
                 chatBox.text = "";
             }
         }
-        if (chatBox.isFocused)
+        else
         {
-            if (Input.GetKeyDown(KeyCode.Return))
+            if (!chatBox.isFocused && Input.GetKeyDown(KeyCode.Return))
+                chatBox.ActivateInputField();
+        }
+
+        if (!chatBox.isFocused)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
             {
-                SendMessageToChat("you pressed the space Key");
+                SendMessageToChat("You pressed the spacebar");
                 Debug.Log("Space");
             }
         }
-
     }
      
     public void SendMessageToChat(string text)
@@ -65,6 +68,34 @@ public class GameManager : MonoBehaviour
 
         MessageList.Add(newMessage);
     }
+    //public void SendMessageToRasa(string text)
+    //{
+    //    // Create a json object from user message
+    //    PostMessageJson postMessage = new PostMessageJson
+    //    {
+    //        sender = "user",
+    //        message = text
+    //    };
+
+    //    string jsonBody = JsonUtility.ToJson(postMessage);
+    //    print("User json : " + jsonBody);
+
+    //    // Create a post request with the data to send to Rasa server
+    //    StartCoroutine(PostRequest(rasa_url, jsonBody));
+    //}
+
+    //private IEnumerator PostRequest(string url, string jsonBody)
+    //{
+    //    UnityWebRequest request = new UnityWebRequest(url, "POST");
+    //    byte[] rawBody = new System.Text.UTF8Encoding().GetBytes(jsonBody);
+    //    request.uploadHandler = (UploadHandler)new UploadHandlerRaw(rawBody);
+    //    request.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+    //    request.SetRequestHeader("Content-Type", "application/json");
+
+    //    yield return request.SendWebRequest();
+
+    //    Debug.Log("Response: " + request.downloadHandler.text);
+    //}
 }
 
 [System.Serializable]
@@ -72,4 +103,10 @@ public class Message
 {
     public string text;
     public Text textObject;
+}
+
+public class PostMessageJson
+{
+    public string message;
+    public string sender;
 }
